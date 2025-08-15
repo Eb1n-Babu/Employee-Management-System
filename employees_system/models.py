cf0c1623fd9c1c5df6fc18b4e5772d8ffe524dbd
Eb1n-Babu <ebin.babu.in@icloud.com>
2025-08-15 14:51:14 +0530

    Django project setup with authentication models and JWT config

--- employees_system/models.py
+++ employees_system/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+import jsonfield
 
 # Create your models here.
+
+class User(AbstractUser):
+    profile_data = jsonfield.JSONField(default=dict)
+
+class FormField(models.Model):
+    label = models.CharField(max_length=255)
+    input_type = models.CharField(max_length=50, choices=[
+        ('text', 'Text'), ('number', 'Number'), ('date', 'Date'), ('password', 'Password')
+    ])
+    order = models.IntegerField(default=0)
+
+class Employee(models.Model):
+    data = jsonfield.JSONField(default=dict)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
