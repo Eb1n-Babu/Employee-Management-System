8baf7ade75823cfe8c09d7374f78084bd49a4b12
Eb1n-Babu <ebin.babu.in@icloud.com>
2025-08-17 22:56:55 +0530

    revert some changes

--- static/js/employee_create.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('employee-form').addEventListener('submit', (e) => {
-    e.preventDefault();
-    fetch(`{% if pk %}/employee/update/{{ pk }}/{% else %}/employee/create/{% endif %}`, {
-        method: 'POST',
-        body: new FormData(e.target),
-    }).then(response => response.json()).then(data => console.log(data));
-});
